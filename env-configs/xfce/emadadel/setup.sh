c7d2f2667a0f2015e457cdb1a350d29124374875
Emad Adel <emadadel1920@gmail.com>
2025-04-23 10:10:03 +0200

    Update setup.sh

--- env-configs/xfce/emadadel/setup.sh
+++ env-configs/xfce/emadadel/setup.sh
@@ -56,6 +56,8 @@ read -p $'\033[1;33m[i] Do you want to install virt-manager? (y/n): \033[0m' vir
 if [[ "$virt_answer" =~ ^[Yy]$ ]]; then
     sudo xbps-install -S libvirt virt-manager virt-manager-tools qemu qemu-ga
     sudo ln -s /etc/sv/libvirtd /var/service/
+    sudo ln -s /etc/sv/virtlockd /var/service/
+    sudo ln -s /etc/sv/virtlogd /var/service/
     sudo usermod -aG libvirt $(whoami)
     sudo usermod -aG kvm $(whoami)
 fi

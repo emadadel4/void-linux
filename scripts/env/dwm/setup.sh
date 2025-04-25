cb8768542c5037e8d12f4d3eb3a2d95d92bca617
Emad Adel <emadadel1920@gmail.com>
2025-04-25 09:22:54 +0300

    Update setup.sh

--- scripts/env/dwm/setup.sh
+++ scripts/env/dwm/setup.sh
@@ -6,6 +6,8 @@ sudo xbps-install -Su -y
 echo "[+] Installing DWM dependencies..."
 sudo xbps-install -S make libX11-devel libXft-devel libXinerama-devel glib-devel font-inconsolata-otf git gcc make xorg-server xinit xorg xauth  -y 
 
+libX11-devel libXft-devel libXinerama-devel
+
 echo "[+] Cloning suckless tools..."
 mkdir -p ~/src
 cd ~/src
@@ -13,19 +15,19 @@ git clone https://git.suckless.org/dwm
 git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/st
 
-echo "[+] Building and installing dwm..."
-cd ~/src/dwm
-cp config.def.h config.h
-make clean install
-
 echo "[+] Building and installing st..."
 cd ~/src/st
 cp config.def.h config.h
-make clean install
+sudo make clean install
 
 echo "[+] Building and installing dmenu..."
 cd ~/src/dmenu
-make clean install
+sudo make clean install
+
+echo "[+] Building and installing dwm..."
+cd ~/src/dwm
+cp config.def.h config.h
+sudo make clean install
 
 echo "[+] Creating .xinitrc..."
 echo 'exec dbus-launch --sh-syntax --exit-with-session dwm' > ~/.xinitrc
